62be64cbf9a36a7673442951899746695003b757
Daniel Krüger <45030029+DC2-DanielKrueger@users.noreply.github.com>
2020-12-09 17:46:51 +0100

    Cleanup (#3)
    * added license header
    fixed fail on javadoc task
    
    * change from CR
    
    * Uniform readme
    
    * Update gradle to 6.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,6 +97,7 @@ tasks.withType<Jar>().configureEach {
 
 tasks.javadoc {
     title = "${metadata.readableName} ${project.version} API"
+    isFailOnError = false
 }
 
 /* ******************** checks ******************** */
