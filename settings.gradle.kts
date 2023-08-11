06fa73cc7fdc09189e94a5e4ee1030bd98113515
Silvio Giebl <silvio.giebl@hivemq.com>
2023-08-11 17:58:01 +0200

    Replace utf8 with defaults plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "hivemq-kafka-hello-world-customization"
 
 pluginManagement {
     plugins {
+        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
     }
 }
