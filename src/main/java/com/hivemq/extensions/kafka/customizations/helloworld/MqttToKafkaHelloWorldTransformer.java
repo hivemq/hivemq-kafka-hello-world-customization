c62d2d85c958fc4c9ef49ddf03ca3dfcbfb7b30a
David Sondermann <david.sondermann@hivemq.com>
2026-08-06 15:38:55 +0200

    Replace the license plugin with Spotless

--- src/main/java/com/hivemq/extensions/kafka/customizations/helloworld/MqttToKafkaHelloWorldTransformer.java
+++ src/main/java/com/hivemq/extensions/kafka/customizations/helloworld/MqttToKafkaHelloWorldTransformer.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hivemq.extensions.kafka.customizations.helloworld;
 
 import com.hivemq.extension.sdk.api.annotations.NotNull;
