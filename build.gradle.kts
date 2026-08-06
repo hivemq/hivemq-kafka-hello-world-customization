031e6d0a3bd50705daa14025e605e3d0637a45a3
David Sondermann <david.sondermann@hivemq.com>
2026-08-06 15:38:55 +0200

    Replace the license plugin with Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     alias(libs.plugins.defaults)
-    alias(libs.plugins.license)
+    alias(libs.plugins.spotless)
 }
 
 group = "com.hivemq.extensions.kafka.customizations"
@@ -42,7 +42,8 @@ tasks.withType<Jar>().configureEach {
     )
 }
 
-license {
-    header = rootDir.resolve("HEADER")
-    mapping("java", "SLASHSTAR_STYLE")
+spotless {
+    java {
+        licenseHeaderFile(rootDir.resolve("HEADER"))
+    }
 }
